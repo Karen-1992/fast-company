3ae8b75668b9ea5f1478392f36b810eacdf2be8b
Karen Habuzian <karen.gabuzyan@gmail.com>
2022-06-05 13:37:53 +0300

    finished lesson-15

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import useMocData from "../utils/mockData";
 
 const Main = () => {
-    return <h1>Main</h1>;
+    const { error, initialize, progress, status } = useMocData();
+    const handleClick = () => {
+        console.log("init");
+        initialize();
+    };
+    return (
+        <div className="container mt-5">
+            <h1>Main</h1>;
+            <h3>Инициализаци данных в FireBase</h3>
+            <ul>
+                <li>{status}</li>
+                <li>{progress}</li>
+                {error && <li>{error}</li>}
+            </ul>
+            <button
+                onClick={handleClick}
+                className="btn btn-primary"
+            >
+                Инициализировать
+            </button>
+        </div>
+    );
 };
 
 export default Main;
